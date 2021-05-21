83be5daacedf5f4070057d50a49fa5263cb00c5f
TanYueyin <tanyueyin@outlook.com>
2021-05-22 01:55:54 +0800

    tiny amendments

--- forward_difference.m
+++ forward_difference.m
@@ -1,13 +1,13 @@
 function U = forward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f)
 %FORWARD_DIFFENCE 此处显示有关此函数的摘要
-%       compUte the nUmerical solUtion for U 
-%       x in (0,1) t > 0
+%       compute the numerical solution for the specific PDE systems
+%       mentioned in README.md
 %% 参数的赋值
 T = t_min : tau : t_max;
 X = x_min : h : x_max;
 r = tau / (h * h);
-%% 求解U的数值解
 U = zeros(length(T),length(X));
+%% 求解U的数值解
 U(1,:) = phi(X); %初值条件
 for k = 1 : length(T) - 1
     for j = 1 : length(X)
--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 %% 参数赋值
-tau = 1/20000;
+tau = 1/5000;
 h = 1/100;
 phi = @(x)(cos(pi*x));
 f = @(x)(sin(x));
@@ -11,11 +11,19 @@ X = x_min : h : x_max;
 T = t_min : tau : t_max;
 
 %% 求数值解和真解
-U = forward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
-U_true = analytical_solution(0,1,tau,0,1,h);
+%U = forward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
+U = backward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
+U_true = analytical_solution(t_min,t_max,tau,x_min,x_max,h);
 %% 画图
-Fig = mesh(X,T,U);
+figure(1)
+Fig1 = mesh(X,T,U);%解的图像
 xlabel('$x$','interpreter','latex');
 ylabel('$t$','interpreter','latex');
 zlabel('$u$','interpreter','latex');
-title('Numerical Solution for Forward Finite Difference Method(r=0.5)','Interpreter','latex')
+title(strcat('Numerical Solution for Backward Finite Difference Method(r=',string(tau/(h*h)),')'),'Interpreter','latex')
+figure(2)
+Fig2 = mesh(X,T,U-U_true);%误差分布
+xlabel('$x$','interpreter','latex');
+ylabel('$t$','interpreter','latex');
+zlabel('$Error$','interpreter','latex');
+title(strcat('Numerical Error for Backward Finite Difference Method(r=',string(tau/(h*h)),')'),'Interpreter','latex')
