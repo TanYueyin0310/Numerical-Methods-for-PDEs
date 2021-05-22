a4e192f21f581c1fb907eef98f5dbef8514f0281
TanYueyin <tanyueyin@outlook.com>
2021-05-23 03:24:31 +0800

    feat: implement error order analysis directly by calling the error_analysis function 增加调用errory_analysis函数进行误差阶分析的功能

--- main.m
+++ main.m
@@ -9,21 +9,26 @@ x_min = 0;
 x_max = 1;
 X = x_min : h : x_max;
 T = t_min : tau : t_max;
-
+method_name = "Crank-Nicolson Method";
 %% 求数值解和真解
 %U = forward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
-U = backward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
+%U = backward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
+U = crank_nicolson(t_min,t_max,tau,x_min,x_max,h,phi,f);
 U_true = analytical_solution(t_min,t_max,tau,x_min,x_max,h);
 %% 画图
-figure(1)
+figure()
 Fig1 = mesh(X,T,U);%解的图像
 xlabel('$x$','interpreter','latex');
 ylabel('$t$','interpreter','latex');
 zlabel('$u$','interpreter','latex');
-title(strcat('Numerical Solution for Backward Finite Difference Method(r=',string(tau/(h*h)),')'),'Interpreter','latex')
-figure(2)
+title(strcat("Numerical Solution for ",method_name,"(r=",string(tau/(h*h)),")"),'Interpreter','latex')
+figure()
 Fig2 = mesh(X,T,U-U_true);%误差分布
 xlabel('$x$','interpreter','latex');
 ylabel('$t$','interpreter','latex');
 zlabel('$Error$','interpreter','latex');
-title(strcat('Numerical Error for Backward Finite Difference Method(r=',string(tau/(h*h)),')'),'Interpreter','latex')
+title(strcat("Numerical Error for ",method_name,"(r=",string(tau/(h*h)),")"),'Interpreter','latex')
+%% 误差分析
+h_list = [1/2 1/5 1/10 1/16 1/40 1/50];
+numerical_method = @crank_nicolson;
+[error_max_list,error_l2_list] = error_analysis(t_min,t_max,tau,x_min,x_max,h_list,phi,f,numerical_method,method_name);
