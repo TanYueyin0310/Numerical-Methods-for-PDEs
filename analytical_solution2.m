035d73df298cbb925d4d37d03bf7295bfa1726ae
TanYueyin <tanyueyin@outlook.com>
2021-05-28 00:50:36 +0800

    feat: add files that computes the analytical solution for problem2

---
+++ analytical_solution2.m
@@ -0,0 +1,17 @@
+function U_true = analytical_solution2(t_min,t_max,tau,x_min,x_max,y_min,y_max,h)
+%ANALYTICAL_SOLUTION2 此处显示有关此函数的摘要
+%   此处显示详细说明
+T = t_min : tau : t_max;
+X = x_min : h : x_max;
+Y = y_min : h : y_max;
+U_true = zeros(length(Y),length(X),length(T));
+for n = 1 : length(T)
+    for j = 1 : length(X)
+        for k = 1 : length(Y)
+            U_true(k,j,n) = sin(pi*X(j)) * cos(pi*Y(k)) * exp(-pi*pi/8*T(n)*T(n));
+        end
+    end
+end
+mesh(X,Y,U_true(:,:,1));
+end
+
