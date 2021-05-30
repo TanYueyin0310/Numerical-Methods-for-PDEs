b41163b7ec2d6fc583cabbc0e1aed2d16611b8e6
TanYueyin <tanyueyin@outlook.com>
2021-05-30 13:21:19 +0800

    feat: implementing the 3 parts of the experiment of PDE1 at one time

--- main1.m
+++ main1.m
@@ -1,17 +1,17 @@
 %% 参数赋值
 tau = 1/5000;
 h = 1/100;
-% phi = @(x)(cos(pi*x)); %PDE1问题中的的边值条件
-% f = @(x)(sin(x)); %PDE1的问题中微分方程的右端项
+phi = @(x)(cos(pi*x)); %PDE1问题中的的边值条件
+f = @(x)(sin(x)); %PDE1的问题中微分方程的右端项
 t_min = 0;
 t_max = 1;
 x_min = 0;
 x_max = 1;
-y_min = 0; %PDE2
-y_max = 1;
 X = x_min : h : x_max;
 T = t_min : tau : t_max;
-method_name = "Crank-Nicolson Method";
+h_list = [1/2 1/5 1/10 1/16 1/40];
+numerical_method = {@crank_nicolson @forward_difference @backward_difference};
+method_name = ["Crank-Nicolson Method","Forward Finite Difference Method","Backward Finite Difference Method"];
 %% 求数值解和真解
 %U = forward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
 %U = backward_difference(t_min,t_max,tau,x_min,x_max,h,phi,f);
@@ -31,6 +31,6 @@ ylabel('$t$','interpreter','latex');
 zlabel('$Error$','interpreter','latex');
 title(strcat("Numerical Error for ",method_name,"(r=",string(tau/(h*h)),")"),'Interpreter','latex')
 %% 误差分析
-h_list = [1/2 1/5 1/10 1/16 1/40 1/50];
-numerical_method = @crank_nicolson;
-[error_max_list,error_l2_list] = error_analysis(t_min,t_max,tau,x_min,x_max,h_list,phi,f,numerical_method,method_name);
+for i = 1 : length(numerical_method)
+    error_analysis1(t_min,t_max,tau,x_min,x_max,h_list,phi,f,numerical_method{i},method_name(i));
+end
